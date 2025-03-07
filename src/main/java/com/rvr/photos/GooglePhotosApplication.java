0d1f124205b5e0a64b82b0e51eb2d57a2f3f240a
roshanvrazak <roshanvrazak@gmail.com>
2025-03-07 22:24:16 +0000

    Initial commit

---
+++ src/main/java/com/rvr/photos/GooglePhotosApplication.java
@@ -0,0 +1,13 @@
+package com.rvr.photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePhotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rvr/photos/GooglePhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvr.photos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
